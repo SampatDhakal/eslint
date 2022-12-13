8989523e164f338fc0e6552a3682f0ce007bede3
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-12-13 12:15:42 +0545

    first commit

---
+++ my-file.js
@@ -0,0 +1,17 @@
+const array = [
+    1, 2, 3
+]
+
+const name = "bob"
+
+// console.log(name);
+
+function doSomeStuff(
+    myArray) {
+    for (let number of
+        myArray) {
+        console.log(number);
+    }
+}
+
+// doSomeStuff(array);
